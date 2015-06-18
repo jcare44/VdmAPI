868b3a15324c762da2c28916cd6fadeb242f6012
jcare44 <ilovedecone44@hotmail.fr>
2015-06-18 07:42:03 +0200

    Refactored parser & tests

--- src/AppBundle/Parser/VdmPageParser.php
+++ src/AppBundle/Parser/VdmPageParser.php
@@ -2,26 +2,36 @@
 namespace AppBundle\Parser;
 
 use Symfony\Component\DomCrawler\Crawler;
+use AppBundle\Entity\Post;
 
 class VdmPageParser {
-    private $crawler;
-
-    public function __construct(Crawler $crawler) {
-        if($crawler === null) {
-            throw new Exception('Null forbiden.');
-        }
-
-        $this->crawler = $crawler;
-    }
-
     /**
      * Crawl the given page for posts and return them
      *
-     * @return Post[] The posts in the given page
+     * @param Crawler $crawler The crawler containing a vdm page
+     * @return Post[]          The posts in the given page
      */
-    public function getPosts() {
-        return $this->crawler->filter('.post.article')->each(function($node) {
-            return (new VdmPostParser($node))->getPost();
+    public static function getPosts(Crawler $crawler) {
+        return $crawler->filter('.post.article')->each(function($node) {
+            return self::getPost($node);
         });
+    }
+
+    /**
+     * Parse the given page post element
+     *
+     * @param Crawler $node The crawler containing a vdm post
+     * @return Post         The parsed post
+     */
+    public static function getPost(Crawler $node) {
+        $additionalInfos = $node->filter('.date > .right_part > p:nth-child(2)')->text();
+
+        $post = new Post();
+        $post->setId($node->attr('id'))
+            ->setContent($node->filter('p')->text())
+            ->setAuthor(trim(preg_replace('/^.*?\- par ([^(]+)\(?.*$/', '$1', $additionalInfos)))
+            ->setPublishedAt(\DateTime::createFromFormat('d/m/Y', preg_replace('/^Le ([0-9\/]+).*$/', '$1', $additionalInfos)));
+
+        return $post;
     }
 }
--- src/AppBundle/Tests/Parser/VdmPageParserTest.php
+++ src/AppBundle/Tests/Parser/VdmPageParserTest.php
@@ -6,11 +6,7 @@ use AppBundle\Parser\VdmPageParser;
 
 class VdmPageParserTest extends \PHPUnit_Framework_TestCase
 {
-    public function testGetPosts()
-    {
-        $crawler = new Crawler(file_get_contents(__DIR__.'/vdmPageParserTest.html'));
-        $posts = (new VdmPageParser($crawler))->getPosts();
-
+    private function assertPosts($posts) {
         $this->assertEquals('17/06/2015', $posts[0]->getPublishedAt()->format('d/m/Y'));
         $this->assertEquals('tartefraise', $posts[0]->getAuthor());
         $this->assertEquals('Aujourd\'hui, pâtissière depuis quelques années, je lis la commande d\'une cliente. Elle veut une tarte aux fraises, mais sans crème, sans nappage et avec un fond de pâte le plus fin possible parce qu\'elle n\'aime pas trop ça. Une barquette de fraises, quoi. VDM', $posts[0]->getContent());
@@ -18,5 +14,24 @@ class VdmPageParserTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('17/06/2015', $posts[1]->getPublishedAt()->format('d/m/Y'));
         $this->assertEquals('E-miel', $posts[1]->getAuthor());
         $this->assertEquals('Aujourd\'hui, et depuis quelques semaines, je demande régulièrement par courriel une semaine de vacances à ma patronne, sans aucune réponse. En revanche, elle a répondu dans la minute lorsqu\'il fut sujet d\'heures supplémentaires. VDM', $posts[1]->getContent());
+    }
+
+    public function testGetPost()
+    {
+        $node = new Crawler(utf8_decode(file_get_contents(__DIR__.'/vdmPost0.html')));
+        $posts[0] = VdmPageParser::getPost($node);
+
+        $node = new Crawler(utf8_decode(file_get_contents(__DIR__.'/vdmPost1.html')));
+        $posts[1] = VdmPageParser::getPost($node);
+
+        $this->assertPosts($posts);
+    }
+
+    public function testGetPosts()
+    {
+        $crawler = new Crawler(file_get_contents(__DIR__.'/vdmPageParserTest.html'));
+        $posts = VdmPageParser::getPosts($crawler);
+
+        $this->assertPosts($posts);
     }
 }
