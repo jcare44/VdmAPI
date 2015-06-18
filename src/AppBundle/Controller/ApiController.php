e400776ac8da2e9c71d08df23727736b2ea0b803
jcare44 <ilovedecone44@hotmail.fr>
2015-06-18 10:30:42 +0200

    Added GET /post/{id} HAndled no content

--- src/AppBundle/Controller/ApiController.php
+++ src/AppBundle/Controller/ApiController.php
@@ -14,14 +14,40 @@ class ApiController extends Controller
     /**
      * @Route("/posts/{pageNumber}", name="api_posts", requirements={"_method"="GET", "pageNumber"="[1-9][0-9]*"})
      */
-    public function messagesAction($pageNumber = 1)
+    public function getPostsAction($pageNumber = 1)
     {
         $posts = $this->getDoctrine()
             ->getRepository('AppBundle:Post')
             ->findByPage($pageNumber);
 
         $response = new JsonResponse();
-        $response->setData($posts);
+
+        if($posts) {
+            $response->setData($posts);
+        } else {
+            $response->setStatusCode(JsonResponse::HTTP_NO_CONTENT);
+        }
+
+        return $response;
+    }
+
+    /**
+     * @Route("/post/{postId}", name="api_post", requirements={"_method"="GET", "postId"="[1-9][0-9]*"})
+     */
+    public function getPostAction($postId)
+    {
+        $post = $this->getDoctrine()
+            ->getRepository('AppBundle:Post')
+            ->find($postId);
+
+        $response = new JsonResponse();
+
+        if($post) {
+            $response->setData($post);
+        } else {
+            $response->setStatusCode(JsonResponse::HTTP_NO_CONTENT);
+        }
+
         return $response;
     }
 }
