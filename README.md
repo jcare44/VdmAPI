45d49de44d262f17cef0205837781f74882ea8a9
jcare44 <ilovedecone44@hotmail.fr>
2015-06-18 16:48:45 +0200

    Imroved documentation

--- README.md
+++ README.md
@@ -25,8 +25,30 @@ $ php app/console vdm:scrape
 
 ###API
 Once your server started, you can access your data through two API calls:
+
 ```
 GET /api/v1/posts/{pageNumber}
+```
+Get a piece of the stored VDM posts.
+Each call get a page of 100 VDM posts maximum.
+For more data, iterate over the pages
+
+Note: The `{pageNumber}` is optional and start at 1.
+
+Returns "304 No Content" when there is no data in the page. In case of available data, the result is a JSON array of Post objects (schema described just bellow)
+
+---
+```
 GET /api/v1/post/{id}
 ```
-Note: The `{pageNumber}` is optional and start at 1.
+Get a specific VDM post
+
+Returns "404 Not Found" when no corresponding data is found, or a Post object following this schema:
+```
+{
+    id: int,
+    content: string,
+    author: string,
+    publishedAt: datetime('Y-m-d H:i:s')
+}
+```
--- src/AppBundle/Controller/ApiController.php
+++ src/AppBundle/Controller/ApiController.php
@@ -12,7 +12,15 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 class ApiController extends Controller
 {
     /**
+     * Get a piece of the stored VDM posts
+     * Each call get a page of 100 VDM posts maximum
+     * For more data, iterate over the pages
+     *
      * @Route("/posts/{pageNumber}", name="api_posts", requirements={"_method"="GET", "pageNumber"="[1-9][0-9]*"})
+     *
+     * @param  integer $pageNumber The optional page number (starting at 1)
+     * @return JsonResponse        The asked page of data (100/page max) in JSON format
+     *                             Returns "304 No Content" when there is no data in the page
      */
     public function getPostsAction($pageNumber = 1)
     {
@@ -32,7 +40,13 @@ class ApiController extends Controller
     }
 
     /**
+     * Get a specific VDM post
+     *
      * @Route("/post/{postId}", name="api_post", requirements={"_method"="GET", "postId"="[1-9][0-9]*"})
+     *
+     * @param  integer $postId The id of the wanted VDM post
+     * @return JsonResponse    The asked VDM post in JSON format
+     *                         Returns "404 Not Found" when no corresponding data is found
      */
     public function getPostAction($postId)
     {
@@ -45,7 +59,7 @@ class ApiController extends Controller
         if($post) {
             $response->setData($post);
         } else {
-            $response->setStatusCode(JsonResponse::HTTP_NO_CONTENT);
+            $response->setStatusCode(JsonResponse::HTTP_NOT_FOUND);
         }
 
         return $response;
